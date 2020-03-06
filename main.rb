309dba92ca1ccb9936d10c48d8877b1eb8b80a3d
Javier Botero <javierbotero10271@gmail.com>
2020-03-06 15:41:23 -0500

    Starting to build the algorith

--- main.rb
+++ main.rb
@@ -1,3 +1,13 @@
-def hello
-  puts 'Hello'
+def bubble_sort (arr)
+  i = 0
+  while i < arr.length - 1
+    puts "While is true"
+    if arr[i] > arr[i + 1]
+      arr[i], arr[i+1] = arr[i+1], arr[i]
+    end
+  i += 1
+  end
+  return arr
 end
+
+puts bubble_sort([23, 4, 11, 10])
